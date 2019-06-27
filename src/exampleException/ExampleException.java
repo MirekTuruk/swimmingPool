6f96ab33656f01f6305742cd3fa8125387079038
MirekTuruk <miroslaw.turuk@gmail.com>
2019-06-27 14:53:55 +0200

    firstMessage

--- src/exampleException/ExampleException.java
+++ src/exampleException/ExampleException.java
@@ -1,25 +1,47 @@
 package exampleException;
 
-import java.util.Scanner;
 
 public class ExampleException {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+//        w tej tablicy podaj słownie indeksy które maja być wypisane na liście "q" - aby zakończyć program,
+//        np. "1" , "2", "jeden", "10", "-10"
 
-        String [] strings = new  String[] {"To", "jest", "bardzo", "przydatny", "kurs"};
+        String[] indexToPrint = new String[]{"1", "2", "3", "10", "jeden", "-1", "dwa", "q"};
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj numer wyrazu, który chcesz zobaczyć (od 0 do 5, 'q' aby zakończyć): ");
+        String[] strings = new String[]{"To", "jest", "bardzo", "przydatny", "kurs"};
 
-        for(int i = 0; i < 100; i++){
-            String next = scanner.next();
-            System.out.println("Podałeś: " + next);
+        for (int i = 0; i < 100; i++) {
+            String next = indexToPrint[i];
+            System.out.println("Pobrano wyraz: " + next);
 
-            if(next.equals("q")){
+            if (next.equals("q")) {
                 break;
             }
-            int index = Integer.valueOf(next);
-            System.out.println("Wyraz o indeksie: " + index + " to: " + strings[index] + "'");
+            try {
+                int index = getIndex(strings, next);
+                System.out.println("Wyraz o indeksie: " + index + " to: " + strings[index] + "'");
+
+            } catch (IllegalArgumentException ex) {
+                System.out.println("Wystąpił błąd: " + ex.getMessage());
+            }
         }
+    }
+
+    public static int getIndex(String[] strings, String next) throws IllegalArgumentException {
+
+            int index = 0;
+            try {
+                index = Integer.valueOf(next);
+            }catch (NumberFormatException ex){
+                throw new IllegalArgumentException("Nie wprowadzono cyfry !");
+            }
+        if(index < 0){
+            throw new IllegalArgumentException("Wprowadzona liczba musi być większa od 0 !");
+        }
+        if(index >= strings.length){
+            throw new IllegalArgumentException("Wprowadzona liczba musi być mniwjsza od " + strings.length + " !");
+        }
+        return index;
     }
 }
