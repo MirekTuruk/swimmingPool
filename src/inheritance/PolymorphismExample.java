328aa4a6e0131893e24f2585169803d7a6dd1b87
MirekTuruk <miroslaw.turuk@gmail.com>
2019-06-27 13:14:12 +0200

    firstMessage

---
+++ src/inheritance/PolymorphismExample.java
@@ -0,0 +1,25 @@
+package inheritance;
+
+import example.AbstractShape;
+import example.EColor;
+
+public class PolymorphismExample {
+    public static void main(String[] args) {
+
+        AbstractShape shape1 = new Rectangle(2, 5, EColor.RED);
+        AbstractShape shape2 = new Sqare(6, EColor.YELLOW);
+        AbstractShape shape3 = new Triangle(3, 4, 5, EColor.ORANGE);
+        AbstractShape shape4 = new Circle(3,EColor.BLUE);
+
+        AbstractShape [] shapes = {shape1, shape2, shape3, shape4};
+
+        for( AbstractShape shape : shapes){
+            System.out.println(shape);
+            System.out.println("perimeter= " + shape.getPerimeter());
+            System.out.println("surface= " + shape.getSurface());
+            System.out.println("============================================");
+        }
+        shape1 = new Circle(10, EColor.YELLOW);
+        System.out.println(shape1);
+    }
+}
