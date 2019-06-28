48598e25ed20ba042810b32cc540989926235fd8
MirekTuruk <miroslaw.turuk@gmail.com>
2019-06-28 11:58:29 +0200

    firstMessage

---
+++ src/timePattern/Main.java
@@ -0,0 +1,17 @@
+package timePattern;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LocalDateTime dateTime = LocalDateTime.of(2010, 10, 12,
+                13, 41, 21);
+
+        DateTimeFormatter customDateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy-HH:mm");
+
+        System.out.println(dateTime.format(customDateTimeFormatter));
+    }
+}
